2ed5fd53f038464b3524198ee70d4b27ff17ce60
Theo <purplecatfishz@aol.com>
2022-09-15 00:21:53 -0400

    made Title.js have consistent spacing with rest of components

--- src/Title.js
+++ src/Title.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
 const Title = ({ text }) => (
     <div className="title">{text}</div>
 )
 Title.propTypes = {
   text: PropTypes.string.isRequired
 }
+
 export default Title
